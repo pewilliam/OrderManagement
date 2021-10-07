a5f50a23debbe71d8fdb643457c969237934c0f1
Pedro William <axlwilliamrose5@gmail.com>
2021-10-07 18:41:20 -0300

    Program to manage order(s)

---
+++ OrderManagment/Entities/Order.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using OrderManagment.Entities.Enums;
+using System.Text;
+
+namespace OrderManagment.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem order)
+        {
+            Items.Add(order);
+        }
+
+        public void RemoveItem(OrderItem order)
+        {
+            Items.Add(order);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem obj in Items)
+            {
+                sum += obj.SubTotal();
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("\nOrder items: ");
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price R$: " + Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ OrderManagment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using OrderManagment.Entities;
+using OrderManagment.Entities.Enums;
+
+namespace OrderManagment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Enter client's data----");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string clientEmail = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("----Enter order data----");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, clientEmail, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items in this order: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("----Enter item #" + i + " data----");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.Clear();
+            Console.WriteLine("----ORDER SUMMARY----");
+            Console.WriteLine(order);
+        }
+    }
+}
